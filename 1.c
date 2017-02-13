c4b2d7751c7255581947cdd7fa17492d9ba3e6ee
Lukas Krishnan C M <cm.lukas02@gmail.com>
2017-02-13 22:43:36 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,32 @@
+# Enrich1617
+#include <stdio.h>
+#include<string.h>
+char sort(char **s)
+{
+	char temp[10];
+	strcpy()
+}
+int main()
+{
+	char s[10][10];
+	int n,b;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%s",&s[i]);
+	}
+	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(strlen(s[i])==strlen(s[j]))
+				{
+					if(s[i]<s[j])
+					{
+						sort(s);
+					}
+				}
+		}
+	}
+}
